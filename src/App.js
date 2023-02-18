ddf5b898c8c5b614319c2b2c386db0a7092a6bbc
Akash4095 <akashathnure40@gmail.com>
2023-02-18 19:20:21 +0530

    added header footer and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer/footer';
+import Header from './components/Hedaer/header';
+import Home from './components/Home/home';
+import PageNotFound from './components/PageNotFound/pageNotFound';
+import MovieDetails from './components/MovieDetails/movieDetails';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div>
-
-      </div>
+      <BrowserRouter>
+        <Header></Header>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/movie/:imdbID' element={<MovieDetails />} />
+            <Route path='/pageNotFound' element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "5bdd0dd7-35d9-4e4f-b589-6dc680785429"
