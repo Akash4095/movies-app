b390e85f543c2e47a0a8628e8285864676073a20
Akash4095 <akashathnure40@gmail.com>
2023-02-24 16:57:22 +0530

    completed listing, movie card,and details page

--- src/components/MovieDetails/movieDetails.js
+++ src/components/MovieDetails/movieDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchAsyncMovieOrShowDetails, getSelectedMovieOrShows } from '../../redux/moviesReducer/movieSlice';
+import { fetchAsyncMovieOrShowDetails, getSelectedMovieOrShows, removeSelectedMOvieOrShow } from '../../redux/moviesReducer/movieSlice';
 import { Icon } from 'semantic-ui-react';
 import "./movieDetails.scss"
 
@@ -15,57 +15,67 @@ const MovieDetails = () => {
 
     useEffect(() => {
         dispatch(fetchAsyncMovieOrShowDetails(imdbID))
+        return () => {
+            dispatch(removeSelectedMOvieOrShow())
+        }
     }, [dispatch, imdbID])
 
     return (
         <div className='movie-section'>
-            <div className='section-left'>
-                <div className='movie-title'>
-                    {data.Title}
-                </div>
-                <div className='movie-rating'>
-                    <span>
-                        IMDB Rating <Icon name='star' color="orange">{data.imdbRating}</Icon>
-                    </span>
-                    <span>
-                        IMDB Votes <Icon name='thumbs up' color="green">{data.imdbVotes}</Icon>
-                    </span>
-                    <span>
-                        Runtime <Icon name='film' color="grey">{data.Runtime}</Icon>
-                    </span>
-                    <span>
-                        Year <Icon name='calendar' color="grey">{data.Year}</Icon>
-                    </span>
-                </div>
-                <div className='movie-plot'>
-                    {data.Plot}
-                </div>
-                <div className='movie-info'>
-                    <div>
-                        <span>Director</span>
-                        <span>{data.Director}</span>
-                    </div>
-                    <div>
-                        <span>Stars</span>
-                        <span>{data.Actors}</span>
-                    </div>
-                    <div>
-                        <span>Geners</span>
-                        <span>{data.Gener}</span>
-                    </div>
-                    <div>
-                        <span>Languages</span>
-                        <span>{data.Language}</span>
-                    </div>
-                    <div>
-                        <span>Awards</span>
-                        <span>{data.Awards}</span>
-                    </div>
-                </div>
-            </div>
-            <div className='section-right'>
-                <img src={data.Poster} alt={data.Title}></img>
-            </div>
+            {
+                Object.keys(data).length !== 0 ?
+                    <>
+                        <div className='section-left'>
+                            <div className='movie-title'>
+                                {data.Title}
+                            </div>
+                            <div className='movie-rating'>
+                                <span>
+                                    IMDB Rating <Icon name='star' color="orange">{data.imdbRating}</Icon>
+                                </span>
+                                <span>
+                                    IMDB Votes <Icon name='thumbs up' color="green">{data.imdbVotes}</Icon>
+                                </span>
+                                <span>
+                                    Runtime <Icon name='film' color="grey">{data.Runtime}</Icon>
+                                </span>
+                                <span>
+                                    Year <Icon name='calendar' color="grey">{data.Year}</Icon>
+                                </span>
+                            </div>
+                            <div className='movie-plot'>
+                                {data.Plot}
+                            </div>
+                            <div className='movie-info'>
+                                <div>
+                                    <span>Director</span>
+                                    <span>{data.Director}</span>
+                                </div>
+                                <div>
+                                    <span>Stars</span>
+                                    <span>{data.Actors}</span>
+                                </div>
+                                <div>
+                                    <span>Geners</span>
+                                    <span>{data.Gener}</span>
+                                </div>
+                                <div>
+                                    <span>Languages</span>
+                                    <span>{data.Language}</span>
+                                </div>
+                                <div>
+                                    <span>Awards</span>
+                                    <span>{data.Awards}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='section-right'>
+                            <img src={data.Poster} alt={data.Title}></img>
+                        </div>
+                    </>
+                    :
+                    <div>Loading...</div>
+            }
         </div>
     )
 }
--- src/redux/moviesReducer/movieSlice.js
+++ src/redux/moviesReducer/movieSlice.js
@@ -47,9 +47,9 @@ const movieSlice = createSlice({
     name: "movies",
     initialState,
     reducers: {
-        addMovies: (state, { payload }) => {
-            state.movies = payload
-        },
+        removeSelectedMOvieOrShow: (state) => {
+            state.movies = {}
+        }
     },
     extraReducers: {
         [fetchAsyncMovies.pending]: () => {
@@ -73,7 +73,7 @@ const movieSlice = createSlice({
     }
 })
 
-export const { addMovies } = movieSlice.actions
+export const { removeSelectedMOvieOrShow } = movieSlice.actions // exporting actions 
 export const getAllMovies = (state) => state.movies.movies
 export const getAllShows = (state) => state.movies.shows
 export const getSelectedMovieOrShows = (state) => state.movies.selectedMovieOrShow
